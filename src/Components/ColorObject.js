f6dc7423ec6626bb46471c2ecbf0fb4e48707c7d
joanterm <jo_joanarg@yahoo.com>
2021-04-14 12:53:31 -0400

    Created separate components for data and styled the app

---
+++ src/Components/ColorObject.js
@@ -0,0 +1,14 @@
+const colorObject = 
+  {
+    green: '#d3fcc9',
+    yellow: '#ffffa8',
+    orange: "#ffffa8",
+    pink: "#ffc0cb",
+    blue: "#ffc0cb",
+    red: "#ffa4a4",
+    violet: "#eeb0f7",
+    sea: "#aad9dd",
+    brown: "#e0c8b1"
+  };
+
+  export default colorObject
